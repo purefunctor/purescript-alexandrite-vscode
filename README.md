6020b8d4a3cefbd5644d791be0b21f34f2500e79
Justin Garcia <git@purefunctor.me>
2026-05-19 11:06:26 +0800

    Import extension code from purefunctor/purescript-alexandrite

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alexandrite for Visual Studio Code
+
+The Alexandrite extension for Visual Studio Code.
---
+++ src/extension.ts
@@ -0,0 +1,48 @@
+import { ExtensionContext, workspace } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  const config = workspace.getConfiguration("purescriptAnalyzer");
+  const serverPath =
+    config.get<string>("serverPath")?.trim() || "purescript-analyzer";
+  const sourceCommand = config.get<string>("sourceCommand");
+
+  const args: string[] = [];
+  if (sourceCommand) {
+    args.push("--source-command", sourceCommand);
+  }
+
+  const serverOptions: ServerOptions = {
+    command: serverPath,
+    args,
+    transport: TransportKind.stdio,
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "purescript" }],
+  };
+
+  client = new LanguageClient(
+    "purescript-analyzer",
+    "PureScript Analyzer",
+    serverOptions,
+    clientOptions,
+  );
+
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
